94ff0728bd2b0edd021e1167e703aa383f580dcb
mantevian <zmantevian@gmail.com>
2025-07-07 00:09:35 +0300

    add banner and fix audio played from js

--- src/util/audio.ts
+++ src/util/audio.ts
@@ -1,10 +1,17 @@
+import hitAudio from "/sound/hit.mp3";
+import coinAudio from "/sound/coin.mp3";
+import explosionAudio from "/sound/explosion.mp3";
+import deadlyAudio from "/sound/deadly.mp3";
+import healAudio from "/sound/heal.mp3";
+import clickAudio from "/sound/click.mp3";
+
 const GameAudio = {
-    hit: () => new Audio("/sound/hit.mp3").play(),
-    coin: () => new Audio("/sound/coin.mp3").play(),
-    explosion: () => new Audio("/sound/explosion.mp3").play(),
-    deadly: () => new Audio("/sound/deadly.mp3").play(),
-    heal: () => new Audio("/sound/heal.mp3").play(),
-    click: () => new Audio("/sound/click.mp3").play()
+    hit: () => new Audio(hitAudio).play(),
+    coin: () => new Audio(coinAudio).play(),
+    explosion: () => new Audio(explosionAudio).play(),
+    deadly: () => new Audio(deadlyAudio).play(),
+    heal: () => new Audio(healAudio).play(),
+    click: () => new Audio(clickAudio).play()
 };
 
 export default GameAudio;
